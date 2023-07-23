03e74e15f128df080f7fe2b33005d5d498f65e35
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-07-22 21:47:42 -0300

    New model (user)

--- app.js
+++ app.js
@@ -14,6 +14,34 @@ app.get('/', (req, res) => {
  res.status(200).json({ msg: 'Port OK' });
 });
 
+//Config JSON response
+app.use(express.json());
+
+//Models
+const User = require('./models/User');
+
+//Register Route
+app.post('/register', async (req, res) => {
+
+ const { 
+  username, 
+  email, 
+  password, 
+  confirmPassword 
+ } = req.body;
+
+ if(!username){
+  return res.status(400).json({ msg: 'Username is required' });
+ }else if(!email){
+  return res.status(400).json({ msg: 'Email is required' });
+ }else if(!password){
+  return res.status(400).json({ msg: 'Password is required' });
+ }else if(!confirmPassword){
+  return res.status(400).json({ msg: 'You must confirm your password!' });
+ }
+
+});
+
 // MongoDB connection
 const DB_NAME = process.env.DB_NAME;
 
